26416fd9f4ede76303f277a9f3ad594959c8ce0c
lursz <lukoszr@gmail.com>
2025-02-21 14:35:03 +0100

    Cube instance draft

--- apps/typegpu-docs/src/content/examples/simulation/gravity/index.ts
+++ apps/typegpu-docs/src/content/examples/simulation/gravity/index.ts
@@ -16,21 +16,58 @@ context.configure({
 const cubeModel = await load('cube.obj', OBJLoader);
 console.log(cubeModel.attributes);
 
-const vertex = d.struct({
+const Vertex = d.struct({
   position: d.location(0, d.vec3f),
   normal: d.location(1, d.vec3f),
   uv: d.location(2, d.vec2f),
 });
 
 
-const vertexLayout = tgpu.vertexLayout((n: number) => d.arrayOf(vertex, n));
+
+const vertexLayout = tgpu.vertexLayout((n: number) => d.arrayOf(Vertex, n));
 const vertexBuffer = root.createBuffer(
-  vertexLayout.schemaForCount(cubeModel.attributes['POSITION'].size / 3)
+  vertexLayout.schemaForCount(cubeModel.attributes['POSITION'].value.length / 3)
 )
-.$usage('vertex')
-.$name('vertex');
+  .$usage('vertex')
+  .$name('vertex');
+vertexBuffer.write([{ position: d.vec3f(0, 1, 2), normal: d.vec3f(0, 0, 0), uv: d.vec2f(0, 0) }]);
 
-// Shaders - vertex shader to build cube and fragment shader to color it
+// Shaders
+const mainVertex = tgpu['~unstable']
+  .vertexFn({
+    in: { position: d.vec3f, normal: d.vec3f, uv: d.vec2f },
+    out: { position: d.builtin.position, color: d.vec4f },
+  })
+  .does((input) => {
+    return {
+      position: d.vec4f(input.position.x, input.position.y, input.position.z - 2, 1),
+      color: d.vec4f(1, 0, 0, 1),
+    };
+  })
+  .$name('mainVertex');
 
+const mainFragment = tgpu['~unstable']
+  .fragmentFn({
+    in: { color: d.vec4f },
+    out: d.location(0, d.vec4f),
+  })
+  .does((input) => {
+    return input.color;
+  })
+  .$name('mainFragment');
 
 // Render pipeline
+const renderPipeline = root['~unstable']
+  .withVertex(mainVertex, vertexLayout.attrib)
+  .withFragment(mainFragment, { format: presentationFormat })
+  .createPipeline();
+
+setTimeout(() => {
+  renderPipeline
+    .withColorAttachment({ view: context.getCurrentTexture().createView(), loadOp: 'clear', storeOp: 'store', clearValue: [1, 0, 0, 1] })
+    .with(vertexLayout, vertexBuffer)
+    .draw(36);
+
+  root['~unstable'].flush();
+  root.destroy();
+}, 200);
