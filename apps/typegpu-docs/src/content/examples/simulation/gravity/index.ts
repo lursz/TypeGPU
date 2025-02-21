4af611714e555846c8843641c1d27aae1680a2b8
lursz <lukoszr@gmail.com>
2025-02-21 13:21:11 +0100

    loaders.gl, implemented cube loading from .obj file

--- apps/typegpu-docs/src/content/examples/simulation/gravity/index.ts
+++ apps/typegpu-docs/src/content/examples/simulation/gravity/index.ts
@@ -1,5 +1,7 @@
 import tgpu from 'typegpu';
 import * as d from 'typegpu/data';
+import { load } from '@loaders.gl/core';
+import { OBJLoader } from '@loaders.gl/obj';
 
 const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
@@ -11,4 +13,24 @@ context.configure({
   alphaMode: 'premultiplied',
 });
 
+const cubeModel = await load('cube.obj', OBJLoader);
+console.log(cubeModel.attributes);
 
+const vertex = d.struct({
+  position: d.location(0, d.vec3f),
+  normal: d.location(1, d.vec3f),
+  uv: d.location(2, d.vec2f),
+});
+
+
+const vertexLayout = tgpu.vertexLayout((n: number) => d.arrayOf(vertex, n));
+const vertexBuffer = root.createBuffer(
+  vertexLayout.schemaForCount(cubeModel.attributes['POSITION'].size / 3)
+)
+.$usage('vertex')
+.$name('vertex');
+
+// Shaders - vertex shader to build cube and fragment shader to color it
+
+
+// Render pipeline
