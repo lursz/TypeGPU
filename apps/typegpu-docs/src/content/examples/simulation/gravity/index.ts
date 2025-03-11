5ee88800b3178c0bc9121814f704d724f5c48d6a
lursz <lukoszr@gmail.com>
2025-03-11 11:09:06 +0100

    Separated logic into files

--- apps/typegpu-docs/src/content/examples/simulation/gravity/index.ts
+++ apps/typegpu-docs/src/content/examples/simulation/gravity/index.ts
@@ -2,8 +2,8 @@ import { load } from '@loaders.gl/core';
 import { OBJLoader } from '@loaders.gl/obj';
 import tgpu from 'typegpu';
 import * as d from 'typegpu/data';
-import { add, dot, max, mul, normalize, pow, sub } from 'typegpu/std';
 import * as m from 'wgpu-matrix';
+import { mainFragment, mainVertex } from './shaders';
 
 const Vertex = d.struct({
   position: d.vec3f,
@@ -22,74 +22,8 @@ const bindGroupLayout = tgpu.bindGroupLayout({
   texture: { texture: 'float' },
   sampler: { sampler: 'filtering' },
 });
-const EXT = bindGroupLayout.bound;
 
-// Shaders
-const sampleTexture = tgpu['~unstable']
-  .fn([d.vec2f], d.vec4f)
-  .does(/*wgsl*/ `(uv: vec2<f32>) -> vec4<f32> {
-    return textureSample(EXT.texture, EXT.sampler, uv);
-  }`)
-  .$uses({ EXT })
-  .$name('sampleShader');
-
-const VertexOutput = {
-    position: d.builtin.position,
-    uv: d.vec2f,
-    normals: d.vec3f,
-    worldPosition: d.vec3f,
-}
-  
-const mainVertex = tgpu['~unstable']
-  .vertexFn({
-    in: { position: d.vec4f, normal: d.vec3f, uv: d.vec2f },
-    out: VertexOutput,
-  })
-  .does((input) => {
-    const camera = EXT.camera.value;
-    const worldPosition = input.position;
-    const relativeToCamera = mul(camera.view, worldPosition);
-
-    return {
-      position: mul(camera.projection, relativeToCamera),
-      uv: input.uv,
-      normals: input.normal,
-      worldPosition: worldPosition.xyz,
-    };
-  })
-  .$name('mainVertex');
-
-const lightPosition = d.vec3f(3.0, 3.0, 2.5);
-const lightDirection = normalize(d.vec3f(2.0, 1.0, 0.5));
-const mainFragment = tgpu['~unstable']
-  .fragmentFn({
-    in: VertexOutput,
-    out: d.vec4f,
-  })
-  .does((input) => {
-    const normal = normalize(input.normals);
-    // Directional lighting
-    const directionalLightIntensity = max(dot(normal, lightDirection), 0.0);
-    const directionalComponent = 0.4 * directionalLightIntensity;
-
-    // Point Lighting
-    const surfaceToLight = normalize(sub(lightPosition, input.worldPosition)); 
-    const pointLightIntensity = max(dot(normal, surfaceToLight), 0.0);
-    const pointComponent = 0.6 * pointLightIntensity;
-
-    const lighting = directionalComponent + pointComponent;
-    const albedo = d.vec3f(1.0, 1.0, 1.0); // base color
-
-    const cameraPos = EXT.camera.value.position;
-    const surfaceToCamera = normalize(sub(EXT.camera.value.position, input.worldPosition));
-
-    const halfVector = normalize(add(surfaceToLight, surfaceToCamera));
-    const specular = pow(max(dot(normal, halfVector), 0.0), 3);
-    return d.vec4f(albedo.x * lighting  * specular, albedo.y * lighting * specular, albedo.z * lighting * specular, 1);
-    // return d.vec4f(specular,specular,specular, 1);
-
-  })
-  .$name('mainFragment');
+export const EXT = bindGroupLayout.bound;
 
 const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 const canvas = document.querySelector('canvas') as HTMLCanvasElement;
@@ -178,6 +112,7 @@ for (let i = 0; i < positions.length / 3; i++) {
 vertexBuffer.write(vertices);
 
 // Render pipeline
+
 const renderPipeline = root['~unstable']
   .withVertex(mainVertex, vertexLayout.attrib)
   .withFragment(mainFragment, { format: presentationFormat })
@@ -256,7 +191,11 @@ function updateCameraOrbit(dx: number, dy: number) {
     d.vec3f(0, 1, 0),
     d.mat4x4f(),
   );
-  cameraBuffer.write({position:newCameraPos.xyz, view: newView, projection: cameraInitial.projection });
+  cameraBuffer.write({
+    position: newCameraPos.xyz,
+    view: newView,
+    projection: cameraInitial.projection,
+  });
 }
 
 canvas.addEventListener('wheel', (event: WheelEvent) => {
@@ -273,7 +212,11 @@ canvas.addEventListener('wheel', (event: WheelEvent) => {
     d.vec3f(0, 1, 0),
     d.mat4x4f(),
   );
-  cameraBuffer.write({position:newCameraPos.xyz, view: newView, projection: cameraInitial.projection });
+  cameraBuffer.write({
+    position: newCameraPos.xyz,
+    view: newView,
+    projection: cameraInitial.projection,
+  });
 });
 
 canvas.addEventListener('mousedown', (event) => {
